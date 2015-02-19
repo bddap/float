fad36e85d3fe15893314ece17f9633efb86b0157
Andrew Dirksen <andrew@dirksen.com>
2015-02-19 15:29:20 -0800

    Lwjgl now loads on mac

---
+++ src/com/dirksen/andrew/Float/Float.java
@@ -0,0 +1,11 @@
+package com.dirksen.andrew.Float;
+
+public class Float {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		new NaitiveTester().run();
+	}
+
+}
