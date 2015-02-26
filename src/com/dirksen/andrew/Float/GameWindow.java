a361663a762b67b778cdf2b98a4c969e94bbdfc4
Andrew Dirksen <andrew@dirksen.com>
2015-02-26 00:30:27 -0800

    Almost rendering a cube correctly.

--- src/com/dirksen/andrew/Float/GameWindow.java
+++ src/com/dirksen/andrew/Float/GameWindow.java
@@ -3,37 +3,9 @@
 package com.dirksen.andrew.Float;
 
 import static org.lwjgl.glfw.Callbacks.errorCallbackPrint;
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_E;
-import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
-import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
-import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
-import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
-import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
-import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
-import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
-import static org.lwjgl.glfw.GLFW.glfwInit;
-import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
-import static org.lwjgl.glfw.GLFW.glfwPollEvents;
-import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
-import static org.lwjgl.glfw.GLFW.glfwSetKeyCallback;
-//import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
-import static org.lwjgl.glfw.GLFW.glfwSetWindowShouldClose;
-import static org.lwjgl.glfw.GLFW.glfwShowWindow;
-import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
-import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
-import static org.lwjgl.glfw.GLFW.glfwTerminate;
-import static org.lwjgl.glfw.GLFW.glfwWindowHint;
-import static org.lwjgl.glfw.GLFW.glfwSetInputMode;
-import static org.lwjgl.glfw.GLFW.glfwGetInputMode;
+import static org.lwjgl.glfw.GLFW.*;
 
-import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
-import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.GL_FALSE;
-import static org.lwjgl.opengl.GL11.GL_TRUE;
-import static org.lwjgl.opengl.GL11.glClear;
-import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
 import java.nio.ByteBuffer;
@@ -140,8 +112,38 @@ public class GameWindow {
         // creates the ContextCapabilities instance and makes the OpenGL
         // bindings available for use.
         GLContext.createFromCurrent();
+        
+        initGl();
 	}
 	
+	private void initGl() {
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+        GL11.glShadeModel(GL11.GL_SMOOTH);
+        GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f); 
+        GL11.glClearDepth(1.0); 
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
+        GL11.glDepthFunc(GL11.GL_LEQUAL); 
+
+        GL11.glMatrixMode(GL11.GL_PROJECTION); 
+        GL11.glLoadIdentity();
+
+        setFrustum(Math.PI/2);
+
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+        GL11.glHint(GL11.GL_PERSPECTIVE_CORRECTION_HINT, GL11.GL_NICEST);
+	}
+	
+	void setFrustum(double fov){
+		double near = 0.0000001;
+		double far = 1000.0;
+		double aspect = hieght/width;
+		double right = Math.tan(fov/2)*near;
+		double left = -right;
+		double top = right * aspect;
+		double bottom = -top;
+		GL11.glFrustum(left, right, bottom, top, near, far);
+	}
+
 	void hideMouse(){
         glfwSetInputMode(myWindow, GLFW.GLFW_CURSOR, GLFW.GLFW_CURSOR_DISABLED );
 	}
@@ -178,11 +180,14 @@ public class GameWindow {
     	//System.out.println(blu);
         glClearColor(red, gre, blu, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+    	    	
+        GL11.glLoadIdentity();  
         
-        /*GL11.glLoadIdentity();  
         
-        GL11.glTranslatef(0f,0.0f,-7f);             
-        GL11.glRotatef(45f,0.0f,1.0f,0.0f);               
+        GL11.glTranslated(0,0,-10);  
+        GL11.glRotated(Math.sin(age)*1000,0,1.0,0);           
+        //GL11.glRotatef(45f,0.0f,1.0f,0.0f);         
+        //GL11.glRotated(age,0.0,1.0,0.0);               
         //GL11.glColor3f(0.5f,0.5f,1.0f);  
              
         GL11.glBegin(GL11.GL_QUADS);    
@@ -216,7 +221,7 @@ public class GameWindow {
            GL11.glVertex3f( 1.0f, 1.0f, 1.0f);
            GL11.glVertex3f( 1.0f,-1.0f, 1.0f);
            GL11.glVertex3f( 1.0f,-1.0f,-1.0f);
-       GL11.glEnd();  */
+       GL11.glEnd();
 
         glfwSwapBuffers(myWindow); // swap the color buffers
 
